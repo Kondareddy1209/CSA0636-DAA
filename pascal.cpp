44493428e53c38b728b464a2fd95a1894d61c823
Mr.kondareddy.c <118420084+Kondareddy1209@users.noreply.github.com>
2024-03-28 12:32:50 +0530

    Add files via upload

---
+++ pascal.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+int binomialCoeff(int n, int k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+void printPascalTriangle(int numRows) {
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numRows - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", binomialCoeff(i, j));
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int numRows;
+    printf("Enter the number of rows for Pascal's triangle: ");
+    scanf("%d", &numRows);
+    printPascalTriangle(numRows);
+    return 0;
+}
